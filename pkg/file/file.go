a505e019c5f31d7c9e9a1e78ac953d07d0600a76
Don Johnson <dj@codetestcode.io>
2024-04-28 14:30:26 -0700

    updated

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -1,18 +1,43 @@
 package file
 
 import (
+	"bytes"
 	"encoding/base64"
+	"image"
+	"image/jpeg"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/nfnt/resize"
 )
 
 func EncodeToBase64(filePath string) (string, error) {
-	data, err := os.ReadFile(filePath)
+	imgFile, err := os.Open(filePath)
 	if err != nil {
 		return "", err
 	}
-	return base64.StdEncoding.EncodeToString(data), nil
+	defer imgFile.Close()
+
+	// Decode the image.
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		return "", err
+	}
+
+	// Resize to a new width while maintaining the aspect ratio.
+	newImage := resize.Resize(800, 0, img, resize.Lanczos3)
+
+	// Encode the resized image to jpeg format.
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, newImage, &jpeg.Options{Quality: 80}) // Reduce quality to reduce file size
+	if err != nil {
+		return "", err
+	}
+
+	// Convert to base64.
+	base64Image := base64.StdEncoding.EncodeToString(buf.Bytes())
+	return base64Image, nil
 }
 
 // ReadFile reads the content of the given file path and returns it as a byte slice.
